1a727eef882598ed384fbaf387178876f2fecf15
mskim9097 <mskim9097@gmail.com>
2026-04-15 16:48:30 -0700

    fix deploy error

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -11,8 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withCors()
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
