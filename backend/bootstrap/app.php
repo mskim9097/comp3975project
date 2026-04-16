734971d922ac63838855ee85f9a042ac62161304
mskim9097 <mskim9097@gmail.com>
2026-04-15 17:14:56 -0700

    fix: deploy error

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,8 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
-        $middleware->append(\Fruitcake\Cors\HandleCors::class);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
